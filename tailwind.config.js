7f94ddfd7cb552d9f0a983b1af9cfdf8e12272d1
sodahsu <piscean0619@gmail.com>
2025-08-13 11:17:36 +0800

    更新

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+if (window.tailwind) {
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#6C936D",
+          secondary: "#3F5146",
+          accent: "#417991",
+          danger: "#DC3545",
+          warning: "#EAD065",
+          "gray-light": "#F8F9FA",
+          "gray-medium": "#6C757D",
+          "gray-dark": "#495057",
+          border: "#ADB5BD",
+          "green-light": "#C5E0C7",
+          "bg-light": "#F3F3F3",
+          "bg-success": "#D1E7DD",
+          "text-success": "#0F5132",
+          "bg-info": "#CFF4FC",
+          "text-info": "#0C7489",
+          "bg-warning": "#FFF3CD",
+          "text-warning": "#664D03",
+          "bg-danger": "#F8D7DA",
+          "text-danger": "#842029",
+        },
+        fontFamily: {
+          noto: ["Noto Sans TC", "sans-serif"],
+        },
+      },
+    },
+  };
+}
